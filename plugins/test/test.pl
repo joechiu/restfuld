2cebb2577b45f4fd3c5216fda2958d7e9882d631
joechiu <joechiu@users.noreply.github.com>
2015-09-22 13:10:28 +1000

    Update test.pl

--- plugins/test/test.pl
+++ plugins/test/test.pl
@@ -2,9 +2,16 @@ use JSON;
 use Data::Dumper;
 use lib '/srv/restful/lib';
 use u;
+
 my $t1 = _gettime;
+
+# global params
 $h = $params || { hello => 'world' };
+
 my $t2 = _gettime;
 $h->{realtime} = _timediff($t1,$t2);
+
+# log the results 
 logit "Results: "._to_json($h);
+# return results as json
 _to_json($h);
